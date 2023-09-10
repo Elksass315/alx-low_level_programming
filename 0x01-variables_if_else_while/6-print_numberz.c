2caf7ef6b0fd88fe1687c4516690d87296573c97
Elksass315 <qsasomar@gmail.com>
2023-09-11 01:03:26 +0300

    Elksass

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all single
+ * digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int c = '0';
+
+	for (; c <= '9'; c++)
+		putchar((char)c);
+	putchar('\n');
+	return (0);
+}
