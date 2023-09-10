f604c5e92976decc5e0aa5d74fab3273e05c394d
Elksass315 <qsasomar@gmail.com>
2023-09-10 15:11:25 +0300

    Elksass

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point print a string
+ *
+ *
+ * Return: Always 1 (fail)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
