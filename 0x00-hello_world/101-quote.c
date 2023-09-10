2cbf2102a53ed0e8d920765b3628fc417d964126
Elksass315 <qsasomar@gmail.com>
2023-09-10 15:21:30 +0300

    Elksass

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - Entry point print a string
@@ -10,6 +9,6 @@
 int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    	write(STDERR_FILENO, str, 52);
+	fprintf(stderr, str);
 	return (1);
 }
