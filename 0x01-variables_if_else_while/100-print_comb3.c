c9bd5585e3b77c86d11d6635c955cef953cdabdd
Elksass315 <qsasomar@gmail.com>
2023-09-11 14:12:13 +0300

    Elksass

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible different combinations of two digits.
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int c = '0';
+
+	for (; c < '9'; c++)
+	{
+		int s = c + 1;
+		for (; s <= '9'; s++)
+		{			
+			putchar((char)c);
+			putchar((char)s);
+			if ((char)c == '8' && s == '9')
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
