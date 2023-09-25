92720f1ab50b0440a72635586bf90ee3969e0a6c
Elksass315 <qsasomar@gmail.com>
2023-09-25 16:48:49 +0300

    Elksass

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,8 +3,8 @@
  * main - program that prints its name, followed by a new line.
  *
  * Args:
- * 	@argc: thr arg count.
- * 	@argv: the arg vectore.
+ *	@argc: thr arg count.
+ *	@argv: the arg vectore.
  * Return: Always 0.
  */
 int main(int argc, char **argv)
