cf59e3075d5753927087db0a59826fb10e101036
Elksass315 <qsasomar@gmail.com>
2023-10-08 23:23:21 +0300

    Elksass

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
-	if (action == NULL)
+	if (action == NULL || array == NULL)
 		return;
 	for (; i < size; i++)
 		(*action)(array[i]);
